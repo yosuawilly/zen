af58181fdc36d19aeb70af37e26983a9a668e92b
yosuawilly <handikayosuawilly@yahoo.co.id>
2013-12-05 20:08:25 -0800

    resize background, and modified splashscreen

--- BelajarShalat/src/com/belajar/shalat/SplashActivity.java
+++ BelajarShalat/src/com/belajar/shalat/SplashActivity.java
@@ -5,6 +5,8 @@ import com.belajar.shalat.activity.MainMenuActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SplashActivity extends Activity{
 	
@@ -36,5 +38,25 @@ public class SplashActivity extends Activity{
 		
 		timer.start();
 	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		
+		unbindDrawables(findViewById(R.id.splash));
+		System.gc();
+	}
+	
+	private void unbindDrawables(View view) {
+        if (view.getBackground() != null) {
+            view.getBackground().setCallback(null);
+        }
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                unbindDrawables(((ViewGroup) view).getChildAt(i));
+            }
+            ((ViewGroup) view).removeAllViews();
+        }
+    }
 
 }
